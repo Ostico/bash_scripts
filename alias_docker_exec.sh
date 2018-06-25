4fa498b3e9bc79336d175696404d15074e8b40f1
Domenico Lupinetti <Ostico@users.noreply.github.com>
2018-06-25 12:23:54 +0200

    Create alias_docker_exec.sh

---
+++ alias_docker_exec.sh
@@ -0,0 +1,6 @@
+# Edit/create the bash_aliases file:
+#$ vi ~/.bash_aliases
+
+#Define a funciton as alias and use: dcc <docker_hash>
+__docker-connect() { docker exec -it $1 bash -c 'stty rows 50 && stty cols 380 && bash'; }
+alias dcc="__docker-connect"
